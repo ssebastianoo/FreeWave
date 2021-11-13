5c530655b68a08fe2412ee93dd84bb06fe3ead5b
ssebastianoo <sebastianogirotto05@gmail.com>
2021-11-13 17:13:48 +0100

    removed test

--- test.py
+++
@@ -1,9 +0,0 @@
-import youtube_dl
-
-ydl_opts = {
-    'format': 'bestaudio',
-}
-
-ytdl =  youtube_dl.YoutubeDL(ydl_opts)
-info = ytdl.extract_info('https://www.youtube.com/watch?v=BaW_jenozKc', download=False)
-print(info['formats'][0]['url'])
